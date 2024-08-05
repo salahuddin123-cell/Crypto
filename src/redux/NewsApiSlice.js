e13152a51d60a5230678dad633b188997513e87e
salahuddin123-cell <65978025+salahuddin123-cell@users.noreply.github.com>
2024-08-05 17:37:26 +0530

    Update NewsApiSlice.js

--- src/redux/NewsApiSlice.js
+++ src/redux/NewsApiSlice.js
@@ -1,20 +1,19 @@
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-const newsApiHeaders = {
-    'X-BingApis-SDK': 'true',
-    'X-RapidAPI-Key': process.env.REACT_APP_NEWS_KEY,
-    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
-}
-const createRequest = (url) => ({ url})
+const newsApiHeaders =  {
+    'x-rapidapi-key': '6482e15532msh91b527b8d8f948ap16d0a3jsn2b886bbe28c9',
+    'x-rapidapi-host': 'google-news13.p.rapidapi.com'
+  }
+const createRequest = (url) => ({ URL,headers:newsApiHeaders})
 export const newsSlice = createApi({
 
     reducerPath: 'newsSlice',
     baseQuery: fetchBaseQuery({
-        baseUrl:'https://newsapi.org/v2/everything'
+        baseUrl:'https://google-news13.p.rapidapi.com'
     }),
     endpoints: (builder) => ({
         getNews: builder.query({
-            query: ({category}) => createRequest(`https://newsapi.org/v2/everything?q=Apple&from=2024-08-05&sortBy=popularity&apiKey=f0c3d57a81464c7bb27ef68bb0138a26`)
+            query: ({category}) => createRequest(`https://google-news13.p.rapidapi.com/search/facebook`)
         }),
        
     })
